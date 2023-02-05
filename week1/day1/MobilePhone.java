4dc383f53be761b6448ff66469e75199fe3d2258
mdrifai42 <124480227+mdrifai42@users.noreply.github.com>
2023-02-05 09:07:44 +0530

    Add files via upload
    Day1 assignments

---
+++ week1/day1/MobilePhone.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class MobilePhone {
+public static void main(String[] args) {
+	String MobileBrandName = "Oneplus";
+	int modelNo = 7;
+	char mobileLogo = '1';
+	short noOfMobilePiece = 1;
+	long mobileIMEINo = 911233455l;
+	float mobilePrice = 28000.00f;
+	boolean isDamaged = false;
+	System.out.println("My mobile name is "+MobileBrandName + modelNo);
+	System.out.println("Mobile model logo is "+mobileLogo);
+	System.out.println("Total no of pieces are "+noOfMobilePiece);
+	System.out.println("IMEI no of my mobile is "+mobileIMEINo);
+	System.out.println("Mobile phone price is "+mobilePrice);
+	System.out.println("Health status of mobile is "+isDamaged);
+}
+}
---
+++ week1/day1/TwoWheeler.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels = 2;
+	short noOfMirrors = 2;
+	long chasisnumber = 8796574382l;
+	boolean isPunctured = false;
+	String bikename = "Yamaha FZ";
+	double noOfKMS = 81800.88;
+	System.out.println("My bike Name is "+bikename);
+	System.out.println("Total no of kms ride in my bike is "+noOfKMS);
+	System.out.println("Status of bike tyres ? Punctured or Not ? "+isPunctured);
+	System.out.println("Total wheels in my bike is "+noOfWheels +" nos");
+	System.out.println("Total mirrors in my bike is "+noOfMirrors);
+	System.out.println("Chasis no of my bike is "+chasisnumber);
+}
+}
